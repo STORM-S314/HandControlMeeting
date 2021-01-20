033d4efab42f95267e710f7f5bff3e59d1faa556
1832985938@qq.com <1832985938@qq.com>
2021-01-21 01:42:34 +0800

    使用YCrCb空间进行肤色识别，还没有加入机器学习模块

--- HandControlPython/Main.py
+++ HandControlPython/Main.py
@@ -1,39 +1,60 @@
 import cv2 as cv
 import numpy as np
 import Pretreat as pre
+#设置字体
+font = cv.FONT_HERSHEY_SIMPLEX
 #ROI大小
 width, height = 300, 300
 #ROI起始位置
-x0 ,y0 = 0, 0
+x0 ,y0 = 300, 100
 #开启摄像头
 cap = cv.VideoCapture(0)
+
+#如果以当前文件为主函数运行
+if __name__=="__main__":
+    while True:
+        #读取摄像头内容
+        ret, frame = cap.read()
+        #翻转 >0沿y轴翻转,0沿x轴翻转,<0沿x,y同时翻转
+        frame = cv.flip(frame,1)
+        #绘制矩形框
+        frame = cv.rectangle(frame,(x0,y0),(x0+width,y0+height),(0,255,255))
+        #显示原图
+        cv.imshow('frame', frame)
+        #获取ROI区域
+        roi = frame[y0:y0+height, x0:x0+width]
+        #图像预处理
+        res = pre.preTreat(frame,roi)
+        cv.imshow("fourierDescriptor",res[0])
+        #窗口名字,回调函数指针
+        #cv.setMouseCallback('After',printHSV)
+        #opencv等待用户触发事件，等待时间为2ms
+        key = cv.waitKey(2) & 0xFF
+        #按键判断并进行一定的调整
+        #按'j''l''u''j'分别将选框左移，右移，上移，下移
+        #按'q'键退出录像
+        if key == ord('i'):
+            y0 += 5
+        elif key == ord('k'):
+            y0 -= 5
+        elif key == ord('l'):
+            x0 += 5
+        elif key == ord('j'):
+            x0 -= 5
+        #如果用户在这个时间内按下ESC，ASCII码
+        #为27，则跳出循环
+        elif key == 27:
+            break
+    #关闭摄像头
+    cap.release()
+    #关闭所有窗口
+    cv.destroyAllWindows()
+
 #鼠标移动显示坐标位置的HSV颜色通道信息
+#event是CV_EVENT_*变量之一
+#x和y是鼠标指针在图像坐标系的坐标（不是窗口坐标系）
+#flag是CV_EVENT_FLAG的组合
+#param是用户定义的传递到setMouseCallback函数用的参数
 def printHSV(event, x, y, flag, param):
     if event == cv.EVENT_MOUSEMOVE:
-        print(dst[x,y])
-
-while True:
-    ret, frame = cap.read()
-    #翻转 >0沿y轴翻转,0沿x轴翻转,<0沿x,y同时翻转
-    frame = cv.flip(frame,1)
-    #绘制矩形框,最后一个为线条粗细
-    frame = cv.rectangle(frame.copy(),(x0,y0),(width,height),(0,255,255),3)
-
-    ROI = frame[x0 : width, y0 : height]
-    #对ROI区域进行处理
-    dst = pre.preTreat(ROI)
-    #显示原图
-    cv.imshow('After', dst)
-    #显示处理过的图
-    cv.imshow('frame', ROI)
-    
-    cv.setMouseCallback('After',printHSV)
-
-    #opencv等待用户触发事件，等待时间为2ms，如果用户在这个时间内按下ESC，ASCII码
-    #为27，则跳出循环
-    if cv.waitKey(2) == 27:
-        break;
-
-
-cap.release()
-cv.destroyAllWindows()
+        print(roi[x,y])
--- HandControlPython/Pretreat.py
+++ HandControlPython/Pretreat.py
@@ -1,36 +1,30 @@
 import cv2 as cv
 import numpy as np
-
-def preTreat(origin):
-
-    #高斯滤波
-    gaussBlur = cv.GaussianBlur(origin,(3,3),0)
-
-    #肤色检测 二值化
-    #HSV上下限
-    lowerHSVHand = np.array([100,0,100])
-    upperHSVHand = np.array([180,50,255])
-    #转换图像
-    hsvHand = cv.cvtColor(gaussBlur,cv.COLOR_BGR2HSV)
-    #设置掩膜
-    mask = cv.inRange(hsvHand,lowerHSVHand,upperHSVHand)
-    #覆盖合并
-    dst = cv.bitwise_and(gaussBlur,gaussBlur,mask = mask)
-
+import fourierDescriptor as fd
+def preTreat(frame,roi):
+    #肤色提取算法
+    res = skinMask(roi)
+    #肤色提取结果
+    cv.imshow("skinMask",res)
     #设置卷积核
-    kernel = cv.getStructuringElement(cv.MORPH_RECT,(3,3))
+    kernel = cv.getStructuringElement(cv.MORPH_RECT,(4,4))
     #腐蚀膨胀
-    dst = cv.morphologyEx(dst,cv.MORPH_OPEN,kernel)
+    res = cv.morphologyEx(res,cv.MORPH_OPEN,kernel)
+    #傅里叶描述子
+    ret, fourier_result = fd.fourierDesciptor(res)
 
-    #寻找边界
-    edge = cv.Canny(dst,50,200)
-
-    #寻找和提取轮廓
-    allContour = cv.findContours(edge,cv.RETR_TREE,cv.CHAIN_APPROX_NONE)
-
-    contour = allContour[1]
-    #绘制黑色背景
-    backgroundBlack = np.ones(dst.shape,np.uint8)
-    #绘制白色轮廓
-    cv.drawContours(backgroundBlack,contour.reshape(-1,1,2),-1,(255,255,255),1)
-    return backgroundBlack
+    return ret, fourier_result
+#使用YCrCb颜色空间的Cr分量和Otsu法阈值分割算法
+def skinMask(roi):
+    #转换至YCrCb空间
+    YCrCb = cv.cvtColor(roi,cv.COLOR_BGR2YCR_CB)
+    #拆分出Y,Cr,Cb值
+    (y,cr,cb) = cv.split(YCrCb)
+    #高斯滤波
+    cr = cv.GaussianBlur(cr,(7,7),0)
+    #Otsu自适应二值化
+    #threshold返回两个返回值,第二个是二值化的图像
+    _,skin = cv.threshold(cr,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    #覆盖合并,对每个像素值二进制与,提取图像精确的边界
+    res = cv.bitwise_and(roi,roi,mask = skin)
+    return res
